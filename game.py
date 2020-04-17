2daaecc2dc093fd45046a4a4a4074e08b2c9b0e3
Guilherme Luan <trexguiluan@hotmail.com>
2020-04-16 22:16:37 -0300

    Create game.py

---
+++ game.py
@@ -0,0 +1,50 @@
+from random import choice
+from time import sleep
+
+opcão = ' '
+opcão_pc = ['PEDRA', 'PAPEL', 'TESOURA']
+computador = choice(opcão_pc) #Sorteia uma opcão para o computador
+
+def jogo(pc, jogador):
+    print(13*'-=')
+    print(f'Computador jogou {pc} \nJogador Jogou {jogador}')
+    print(13 * '-=')
+
+def resultado(pc, jogador):
+    if computador == 'PEDRA':
+        if opcão == 'PEDRA':
+            print('EMPATE !')
+        elif opcão == 'PAPEL':
+            print('JOGADOR VENCEU! ')
+        elif opcão == 'TESOURA':
+            print('COMPUTADOR VENCEU! ')
+
+    if computador == 'PAPEL':
+        if opcão == 'PAPEL':
+            print('EMPATE')
+        elif opcão == 'TESOURA':
+            print('JOGADOR VENCEU!')
+        elif opcão == 'PEDRA':
+            print('COMPUTADOR VENCEU!')
+
+    if computador == 'TESOURA':
+        if opcão == 'TESOURA':
+            print('EMPATE:')
+        elif opcão == 'PEDRA':
+            print('JOGADOR VENCEU! ')
+        elif opcão == 'PAPEL':
+            print('COMPUTADOR VENCEU! ')
+
+print(f'{"==== PEDRA, PAPEL, TESOURA ===":^30}')
+while opcão not in 'PEDRAPAPELTESOURA':
+    opcão = str(input('Suas opções: \n - PEDRA \n - PAPEL \n - TESOURA \n Qual é a sua escolha? ')).upper().strip()
+
+print('JO')
+sleep(0.3)
+print('KEN')
+sleep(0.3)
+print('PO!!!')
+sleep(0.3)
+
+jogo(computador, opcão)
+resultado(computador, opcão)
